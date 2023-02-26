d70e9b2f6fabe2690a2ea57c829010c7345db4bb
Nikolay Kolentcev <NKolentcev@gmail.com>
2023-02-26 05:27:57 +0300

    main and pin

---
+++ pax_control/lib/screens/main_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:pax_control/screens/pin_screen.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Color(0xfff7f6fb),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 32, horizontal: 32),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/air-logo.png",
+                width: 240,
+              ),
+              SizedBox(
+                height: 48,
+              ),
+              Text(
+                "PAX Control",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "АВИАИТ - информационные технологии aviait.ru",
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 12,
+                    color: Colors.black38),
+              ),
+              SizedBox(
+                height: 38,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => PinScreen()));
+                  },
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.cyan),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(34),
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(14),
+                    child: Text("-= ВХОД =-"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- pax_control/lib/screens/pin_screen.dart
+++ pax_control/lib/screens/pin_screen.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
-class PinScreen extends StatelessWidget {
+class PinScreen extends StatefulWidget {
   const PinScreen({Key? key}) : super(key: key);
 
   @override
+  State<PinScreen> createState() => _PinScreenState();
+}
+
+class _PinScreenState extends State<PinScreen> {
+  TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "Добро пожаловать",
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+            child: Text(
+          "Введите пин (табельный номер)",
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 14, color: Colors.cyan),
+        )),
+        SizedBox(
+          height: 40,
         ),
-      ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 24),
+          child: PinCodeTextField(
+            key: UniqueKey(),
+            appContext: context,
+            controller: controller,
+            length: 6,
+            cursorHeight: 19,
+            //enableActiveFill: true,
+            textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+            pinTheme: PinTheme(
+              shape: PinCodeFieldShape.underline,
+              fieldWidth: 50,
+              inactiveColor: Colors.grey,
+              selectedColor: Colors.blue,
+              activeFillColor: Colors.cyanAccent,
+              selectedFillColor: Colors.cyan,
+              //borderRadius: BorderRadius.circular(8),
+            ),
+            keyboardType: TextInputType.number,
+            onChanged: (value) {
+              print(value.length);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
